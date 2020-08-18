9cf25a66d79db1082b8b684fbb2e00c59482b7b7
Omid Nikrah <omidnikrah@gmail.com>
2020-08-18 23:21:35 +0430

    Update recommender.js

--- assets/js/recommender.js
+++ assets/js/recommender.js
@@ -40,5 +40,4 @@ $(document).ready(function() {
       return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
     }
     var hexDigits = new Array ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
-
 });
